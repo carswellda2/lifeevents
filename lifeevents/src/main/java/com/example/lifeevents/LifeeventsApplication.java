814d881dce1b0b925e845c521ec4d01d88db7045
carswellda2 <carswellda2@gmail.com>
2022-08-23 11:51:02 -0400

    Initial commit of project.

---
+++ lifeevents/src/main/java/com/example/lifeevents/LifeeventsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lifeevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeeventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeeventsApplication.class, args);
+	}
+
+}
---
+++ lifeevents/src/test/java/com/example/lifeevents/LifeeventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lifeevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeeventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
