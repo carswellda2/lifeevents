126274459d9d8901d5e82f44a7c54384e9b91469
carswellda2 <carswellda2@gmail.com>
2022-08-25 13:38:26 -0400

    Daily commit

--- lifeevents/src/main/java/com/example/lifeevents/LifeeventsApplication.java
+++ lifeevents/src/main/java/com/example/lifeevents/LifeeventsApplication.java
@@ -1,16 +1,26 @@
 package com.example.lifeevents;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LifeeventsApplication {
+	
+	final static Logger logger = LogManager.getLogger(LifeeventsApplication.class); 
 
 	public static void main(String[] args) {
 		SpringApplication.run(LifeeventsApplication.class, args);
-		System.out.println("Woot!, there it is .... ");
-		System.out.println("Here'a another change .... ");
-		System.out.println("Making a wednesday change ");
+		
+		logger.debug("Debug statement here .... ");
+		logger.info("First logger info statement ....." );
+		
+		
+		logger.info("Life events application is up and running  .... ");
+		logger.info("This message (and the one above) is generated on application startup.... ");
+	
 	}
 
 }
